0002b7da1d67124908db8a402944355a18993942
artvezhl <artvezhl@gmail.com>
2022-10-06 15:31:11 +0600

    init project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './App.module.scss';
+
+function App() {
+  return (
+    <div className={s.App}>
+        <h1>SWS test</h1>
+    </div>
+  );
+}
+
+export default App;
